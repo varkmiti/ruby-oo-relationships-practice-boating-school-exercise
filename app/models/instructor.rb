62f2c25891867e61173a535ccf0e825d14e02bf5
Mark Viti <+>
2022-06-05 18:05:56 -0400

    commit tree 55cdab94b6f6e33196475f0fb1a67459154f6c4a

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,22 @@
 class Instructor
 
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def all_students
+        BoatingTest.all.filter { |test| test.instructor == self }.collect { |test| test.student }
+    end 
+
+    end 
+    def passed_student 
+        BoatingTest.all.filter { |test| test.instructor == self && test.test_status == "passed" }.collect { |test| test.student }
+    end 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def add_boating_test(student, test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end 
+
+    def self.find_student(name)
+        Student.all.select { |student| student.first_name == name }
+    end 
+
 end
